972a15abb3d7a9da76cdf557bcda5d147d715845
Banhbaosua <angisita12345@gmail.com>
2024-09-26 15:14:10 +0700

    set up UI and script

---
+++ Assets/GAME/Gameplay/State Machine/States/SpeedUpgradeState.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpgradeState : GameplayStateBehaviour
+{
+    public SpeedUpgradeState(GameplayStatemachineController stateMachineController) : base(stateMachineController)
+    {
+    }
+
+    public override void OnStateActivated()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnStateDisabled()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnStateRegistered()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update()
+    {
+        throw new System.NotImplementedException();
+    }
+}
